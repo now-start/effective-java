acd7a6c6a9947b87b5978b51427b3bc819e10ef1
moon <nowstart2691@gmail.com>
2023-07-15 00:13:45 +0900

    Init CI
    * auto update readme.md

--- README.md
+++ README.md
@@ -34,17 +34,17 @@
 
 ## Chapter2
 
-| item                                            | presentation                                                                                                                                                                                                                                                                                    |
-|-------------------------------------------------|-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
-| item1. 생성자 대신 정적 팩터리 메서드를 고려하라                  | [now-start](https://github.com/now-start/effective-java/blob/main/chater02/item1-%EC%83%9D%EC%84%B1%EC%9E%90%20%EB%8C%80%EC%8B%A0%20%EC%A0%95%EC%A0%81%20%ED%8C%A9%ED%84%B0%EB%A6%AC%20%EB%A9%94%EC%84%9C%EB%93%9C%EB%A5%BC%20%EA%B3%A0%EB%A0%A4%ED%95%98%EB%9D%BC-nowstart.md)                 |
-| item2. 생성자에 매개변수가 많다면 필더를 고려하라                  | [now-start](https://github.com/now-start/effective-java/blob/main/chater02/item2-%EC%83%9D%EC%84%B1%EC%9E%90%EC%97%90%20%EB%A7%A4%EA%B0%9C%EB%B3%80%EC%88%98%EA%B0%80%20%EB%A7%8E%EB%8B%A4%EB%A9%B4%20%ED%95%84%EB%8D%94%EB%A5%BC%20%EA%B3%A0%EB%A0%A4%ED%95%98%EB%9D%BC-nowstart.md)           |
-| item3. private 생성자나 열거 타입으로 싱글턴임을 보증하라          | [now-start](https://github.com/now-start/effective-java/blob/main/chater02/item3-private%20%EC%83%9D%EC%84%B1%EC%9E%90%EB%82%98%20%EC%97%B4%EA%B1%B0%20%ED%83%80%EC%9E%85%EC%9C%BC%EB%A1%9C%20%EC%8B%B1%EA%B8%80%ED%84%B4%EC%9E%84%EC%9D%84%20%EB%B3%B4%EC%A6%9D%ED%95%98%EB%9D%BC-nowstart.md) |
-| item4. 인스턴스화를 막으려거든 private 생성자를 사용하라           | [now-start]()                                                                                                                                                                                                                                                                                   |
-| item5. 자원을 직접 명시하지 말고 의존 객체 주입을 사용하라            | [now-start]()                                                                                                                                                                                                                                                                                   |
-| item6. 불필요한 객체 생성을 피하라                          | [now-start]()                                                                                                                                                                                                                                                                                   |
-| item7. 다 쓴 객체 참조를 해제하라                          | [now-start]()                                                                                                                                                                                                                                                                                   |
-| item8. finalizer와 cleaner 사용을 피하라               | [now-start]()                                                                                                                                                                                                                                                                                   |
-| item9. try-finally 보다는 try-with-resources를 사용하라 | [now-start]()                                                                                                                                                                                                                                                                                   |
+| item                                            | presentation |
+|-------------------------------------------------|--------------|
+| item1. 생성자 대신 정적 팩터리 메서드를 고려하라                  |              |
+| item2. 생성자에 매개변수가 많다면 필더를 고려하라                  |              |
+| item3. private 생성자나 열거 타입으로 싱글턴임을 보증하라          |              |
+| item4. 인스턴스화를 막으려거든 private 생성자를 사용하라           |              |
+| item5. 자원을 직접 명시하지 말고 의존 객체 주입을 사용하라            |              |
+| item6. 불필요한 객체 생성을 피하라                          |              |
+| item7. 다 쓴 객체 참조를 해제하라                          |              |
+| item8. finalizer와 cleaner 사용을 피하라               |              |
+| item9. try-finally 보다는 try-with-resources를 사용하라 |              |
 
 ## Chapter3
 
---
+++ util/CreateReadme.java
@@ -0,0 +1,133 @@
+package util;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+enum StaticFinal {
+    EFFECTIVE_JAVA("""
+        
+        # effective-java
+                        
+        [![build](https://github.com/now-start/effective-java/actions/workflows/pages/pages-build-deployment/badge.svg)](https://now-start.github.io/effective-java/)
+                        
+        """),
+    CONVENTION("""
+        
+        ## Convention
+
+        * GitHub Workflow 사용
+        * 각자 필요한 Branch 생성
+        * 각 Chapter 별 패키지에 itemNumber_itemTitle_gitHubUserName 으로 저장
+        * 필요시 각 Chapter 패키지에 src 및 resource 패키지 생성 가능
+        * 작성 완료 후 reviewer 전체 지정 후 main 으로 PR
+        * 질문 및 필요사항 review 후 merge
+                    
+        """),
+    GROUND_RULE("""
+        
+        ## Ground Rule
+                    
+        """),
+    REFERENCE("""
+        
+        ## Reference
+
+        * [책 정보 링크](https://www.yes24.com/Product/Goods/65551284)
+        * 관련 강의
+            - [이펙티브 자바 완벽 공략 1부](https://www.inflearn.com/course/%EC%9D%B4%ED%8E%99%ED%8B%B0%EB%B8%8C-%EC%9E%90%EB%B0%94-1)
+            - [이펙티브 자바 완벽 공략 2부](https://www.inflearn.com/course/%EC%9D%B4%ED%8E%99%ED%8B%B0%EB%B8%8C-%EC%9E%90%EB%B0%94-2)
+        * [이펙티브자바 공식 Github](https://github.com/WegraLee/effective-java-3e-source-code)
+        * [이펙티브자바 3판 번역 용어 설명](https://docs.google.com/document/d/1Nw-_FJKre9x7Uy6DZ0NuAFyYUCjBPCpINxqrP0JFuXk/edit)
+                    
+        """),
+    ITEM("""
+        
+        | item                                            | presentation |
+        |-------------------------------------------------|--------------""");
+
+
+    private final String value;
+
+    StaticFinal(String path) {
+        this.value = path;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+}
+
+enum Path {
+    ROOT_PATH("./"), README_PATH("./readme.md");
+
+    private final String value;
+
+    Path(String path) {
+        this.value = path;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+}
+
+public class CreateReadme {
+    public static void main(String[] args) throws IOException {
+        List<String> chapters = Arrays.stream(new File(Path.ROOT_PATH.toString()).list()).filter(s -> s.contains("chater")).toList();
+        BufferedWriter bw = new BufferedWriter(new FileWriter(Path.README_PATH.toString()));
+
+        bw.write(StaticFinal.EFFECTIVE_JAVA.toString());
+        list(chapters, bw);
+        bw.write(StaticFinal.CONVENTION.toString());
+        bw.write(StaticFinal.GROUND_RULE.toString());
+        bw.write(StaticFinal.REFERENCE.toString());
+        itemList(chapters, bw);
+        bw.flush();
+        bw.close();
+    }
+
+    private static void itemList(List<String> chapters, BufferedWriter bw) throws IOException {
+        for (String chapter : chapters) {
+            bw.write("\n## " + toUpperCase(chapter) + "\n");
+            bw.write(StaticFinal.ITEM.toString());
+
+            List<String> items = Arrays.stream(new File(chapter).list()).filter(s -> s.contains("item")).toList();
+            String flag = "";
+            for (String item : items) {
+                String[] split = item.split("-");
+                String itemNumber = split[0];
+                String itemName = split[1];
+                String userName = split[2];
+
+                if (!flag.equals(itemNumber)) {
+                    bw.write(" |\n");
+                    bw.write(" | " + itemNumber + ". " + itemName + " | [" + userName.split("\\.")[0] + "](https://github.com/now-start/effective-java/blob/main/" + item.replace(" ", "%20") + ")");
+                    flag = itemNumber;
+                } else {
+                    bw.write(" / [" + userName.split("\\.")[0] + "](https://github.com/now-start/effective-java/blob/main/" + item.replace(" ", "%20") + ")");
+                    flag = itemNumber;
+                }
+            }
+            bw.write(" |\n");
+        }
+    }
+
+    private static void list(List<String> chapters, BufferedWriter bw) throws IOException {
+        if (chapters != null) {
+            bw.write("* List\n");
+        }
+        for (String chapter : chapters) {
+            bw.write("    * [" + toUpperCase(chapter) + "](#" + chapter + ")\n");
+        }
+    }
+
+    public static String toUpperCase(String s) {
+        return s.substring(0, 1).toUpperCase() + s.substring(1);
+    }
+}
